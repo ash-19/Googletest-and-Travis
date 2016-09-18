c29d46ba0246f35b2b138a43a7c745a19cf7b811
snehashm <snehashm@lab2-19.eng.utah.edu>
2016-09-18 12:04:19 -0600

    Added Students.h and basic test case

--- StudentsTests.cpp
+++ StudentsTests.cpp
@@ -1,3 +1,23 @@
+#include <stdio.h>
 #include "gtest/gtest.h"
+#include <string>
+#include <vector>
+#include <map>
+#include "Students.h"
 
 // TODO: stub test cases, then fill them
+
+//  Include call to addUser() in the interface
+//  with valid string name and valid unsigned int ID.
+//  Tests if names were indeed added to the map with a
+//  call to nameExists().
+TEST(AddUser, ValidParameters) {
+        Students* studentsDB = new Students();
+        studentsDB->addUser("Pikachu", 1);
+        studentsDB->addUser("Balbasaur", 2);
+        studentsDB->addUser("Charmander", 3);
+        EXPECT_TRUE (studentsDB->nameExists("Pikachu"));
+        EXPECT_TRUE (studentsDB->nameExists("Balbasaur"));
+        EXPECT_TRUE (studentsDB->nameExists("Charmander"));
+        EXPECT_FALSE(studentsDB->nameExists("Squirtel"));
+}
